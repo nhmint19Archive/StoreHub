1102eda92a57a246c26a304fccd8564987bf7644
basbaybee <emperorhuyv@gmail.com>
2023-05-13 22:11:22 +1000

    added xml documentation

--- Assignment3/Assignment3.Domain/Data/Result.cs
+++ Assignment3/Assignment3.Domain/Data/Result.cs
@@ -1,8 +1,27 @@
 ﻿namespace Assignment3.Domain.Data;
+
+/// <summary>
+/// Represents the result of a data transaction.
+/// </summary>
 public enum DataResult
 {
+    /// <summary>
+    /// Successful data transaction.
+    /// </summary>
     Success,
+
+    /// <summary>
+    /// Transaction failed due to conflict with existing data.
+    /// </summary>
     Conflict,
-    CorruptedData,
-    NotFound
+
+    /// <summary>
+    /// Transaction failed because the related entity was not found.
+    /// </summary>
+    NotFound,
+
+    /// <summary>
+    /// Transaction failed because the connection to the data store failed.
+    /// </summary>
+    Disconnected
 }
