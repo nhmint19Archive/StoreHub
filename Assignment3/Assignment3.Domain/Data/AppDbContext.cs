06fbd9be28df8d5119cb01809ec380cdead26726
basbaybee <emperorhuyv@gmail.com>
2023-05-13 22:05:23 +1000

    fixed typo in db connection string added README file detailing project set up

--- Assignment3/Assignment3.Domain/Data/AppDbContext.cs
+++ Assignment3/Assignment3.Domain/Data/AppDbContext.cs
@@ -8,6 +8,6 @@ internal class AppDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(@"Server=\\locabDB;Database=Assignment3;Trusted_Connection=True;");
+        optionsBuilder.UseSqlServer(@"Server=\\localDB;Database=Assignment3;Trusted_Connection=True;");
     }
 }
---
+++ README.md
@@ -0,0 +1,7 @@
+# SWE30003 Assignment 3
+
+## Project set up
+
+1. Install [.NET 7](https://dotnet.microsoft.com/en-us/download/dotnet/7.0) to your local machine:
+2. Run `dotnet restore` to restore dependencies of the project.
+3. Run `dotnet run "/path/to/Assignment3.Application.csproj"` to start the application.
