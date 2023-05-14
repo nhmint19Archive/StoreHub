3134e3e5038bf01c2dc7f18b2b4512f0fdc6c12c
nhmint19 <nguyenhoangminh191202@gmail.com>
2023-05-14 15:36:26 +1000

    initiate accounts classes

---
+++ Assignment3/Assignment3.Domain/Models/CustomerAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.Domain.Models
+{
+    internal class CustomerAccount : UserAccount
+    {
+        private bool _active;
+
+        public CustomerAccount(string id, string username, string password) : base(id, username, password)
+        {
+            _active = true;
+        }
+
+        public bool Active
+        {
+            get => _active; 
+            set => _active = value; 
+        }
+
+        protected override void Authenticate()
+        {
+        }
+
+        public void PlaceOrder()
+        {
+        }
+
+        //public string? searchCatalogue(Catalogue catalogue, string keyword)
+        //{
+        //    return null;
+        //}
+    }
+}
---
+++ Assignment3/Assignment3.Domain/Models/UserAccount.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.Domain.Models
+{
+    internal abstract class UserAccount
+    {
+        private string _id;
+        private string _username;
+        private string _password;
+        private string? _email;
+        private string? _phone;
+        private DateTime? _registryDate;
+        public UserAccount(string id, string username, string password)
+        {
+            _id = id;
+            _username = username;
+            _password = password;
+            _email = null;
+            _phone = null;
+            _registryDate = null;
+        }
+
+        public string Id 
+        {
+            get => _id;
+        }
+
+        public string Username
+        {
+            get => _username;
+            set => _username = value;
+        }
+
+        public string Password
+        {
+            get => _password;
+            set => _password = value;
+        }
+
+        public string? Email
+        {
+            get => _email;
+            set => _email = value;
+        }
+
+        public string? Phone
+        {
+            get => _phone;
+            set => _phone = value;
+        }
+
+        public DateTime? RegistryDate
+        {
+            get => _registryDate;
+        }
+
+        protected abstract void Authenticate();
+    }
+}
