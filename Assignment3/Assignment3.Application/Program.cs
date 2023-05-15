41f367ad12061015e0850150436c43566ceb2234
Lemoonh <anhminhnguyenle291@gmail.com>
2023-05-15 19:37:35 +1000

    create input controller

--- Assignment3/Assignment3.Application/Program.cs
+++ Assignment3/Assignment3.Application/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace Assignment3;
+﻿using Assignment3.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Assignment3;
 
 internal class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        var services = new ServiceCollection();
+        services.AddScoped<IInputController, InputController>();
+        var serviceProvider = services.BuildServiceProvider();
+
+        // Obtain an instance of the InputController object
+        var inputController = serviceProvider.GetService<IInputController>();
     }
 }
---
+++ Assignment3/Assignment3.Application/Services/InputController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Assignment3.Application.Services
+{
+    internal class InputController
+    {
+        public string SanitiseInput(string input)
+        {
+            return input.ToLower().Trim();
+        }
+
+        public bool ValidateCardNumber(string cardNumber)
+        {
+            // Check if the card number matches a valid credit card pattern
+            Regex regex = new Regex(@"^(4|5|6)\d{3}[\ \-]?\d{4}[\ \-]?\d{4}[\ \-]?\d{4}$");
+            if (!regex.IsMatch(cardNumber))
+            {
+                return false;
+            }
+
+            // Remove any spaces or dashes from the card number
+            cardNumber = cardNumber.Replace(" ", "").Replace("-", "");
+
+            // Apply the Luhn algorithm to validate the card number
+            int sum = 0;
+            int length = cardNumber.Length;
+            for (int i = 0; i < length; i++)
+            {
+                int digit = int.Parse(cardNumber[i].ToString());
+
+                if (i % 2 == length % 2)
+                {
+                    digit *= 2;
+                    if (digit > 9) digit -= 9;
+                }
+
+                sum += digit;
+            }
+
+            return sum % 10 == 0;
+        }
+
+        public bool ValidateEmail(string email)
+        {
+            Regex regex = new Regex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+            return regex.IsMatch(email);
+        }
+
+        public bool ValidateInput(string input)
+        {
+            // Input validation logic here
+            return false;
+        }
+    }
+}
+}
