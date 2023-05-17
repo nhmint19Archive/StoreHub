ec9453c8dbe3cc1ed0dead704ddebf750c1db2fb
Lemoonh <anhminhnguyenle291@gmail.com>
2023-05-17 20:57:56 +1000

    initiate models

---
+++ Assignment3/Assignment3.Domain/Models/Invoice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.Domain.Models;
+public class Invoice
+{
+	public int Id { get; set; }
+	public List<Dictionary<Product, uint>> Products;
+	public decimal TotalPrice { get; }
+
+	public Invoice(List<Dictionary<Product, uint>> products, decimal totalPrice)
+	{
+		Products = products;
+		TotalPrice = totalPrice;
+	}
+
+}
---
+++ Assignment3/Assignment3.Domain/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.Domain.Models;
+public class Order
+{
+	public int Id { get; set; }
+	public List<Dictionary<Product, uint>> Products { get; set; }
+	public string Status { get; set; } = "In cart";
+	public string ReceiptId { get; set; } = string.Empty;
+	public string InvoiceId { get; set; } = string.Empty;
+
+	public Order(int id, List<Dictionary<Product, uint>> products)
+	{
+		Id = id;
+		Products = products;
+	}
+
+}
